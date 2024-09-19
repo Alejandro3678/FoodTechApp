cbdc0931db3e0649afcd40058b4c42da9c694d63
Daniel Carrillo <carrilloalejandro3897@gmail.com>
2024-09-18 22:53:02 -0600

    pruebas

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: PantallaInicio(),
+        body: PantallaInicio(), //Basura 1
       ),
     );
   }
